ac09f6c611bfd952bb2924e570e2571242215af7
GeorgeThomas1863 <133590615+GeorgeThomas1863@users.noreply.github.com>
2025-05-06 19:45:31 -0400

    name chnage

--- app.js
+++ app.js
@@ -41,5 +41,5 @@ app.use(express.static("public"));
 app.use(CONFIG.expressPicPath, express.static(CONFIG.picPath));
 app.use(routes);
 
-app.listen(1951);
+app.listen(1950);
 // app.listen(CONFIG.port);
