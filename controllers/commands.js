9f0a435b556ee5177d991cf5aeb669c1e5c2d160
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-05 20:54:37 -0400

    cors working

--- controllers/commands.js
+++ controllers/commands.js
@@ -3,14 +3,15 @@ import CONFIG from "../config/display-config.js";
 export const parseAdminCommand = async (req, res) => {
   //send to other app for parsing
   try {
-    const response = await fetch(CONFIG.apiURL, {
+    const resAPI = await fetch(CONFIG.apiURL, {
       method: "POST",
       body: JSON.stringify(req.body),
       headers: {
         "Content-Type": "application/json",
       },
     });
-    const data = await response.json();
+
+    const data = await resAPI.json();
     return res.json(data);
   } catch (error) {
     console.log(error);
