2bb0d1602f537fc30f713e7546895b02f4d590fb
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-05 20:08:09 -0400

    molasses paced dev

--- controllers/commands.js
+++ controllers/commands.js
@@ -2,7 +2,4 @@ import CONFIG from "../config/display-config.js";
 
 export const parseAdminCommand = async (req, res) => {
   const inputParams = req.body;
-  const { route } = inputParams;
-  console.log("AHHHHHHHHHHH");
-  console.log(req.body);
 };
