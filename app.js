e832d3a119bc0334816c682a4c9b2a2b64ef73a7
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-11 14:08:46 -0400

    displaying

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.use(express.static("public"));
 app.use(CONFIG.expressPicPath, express.static(CONFIG.picPath));
 app.use(routes);
 
-// app.listen(1950);
-app.listen(CONFIG.port);
+app.listen(1801);
+// app.listen(CONFIG.port);
