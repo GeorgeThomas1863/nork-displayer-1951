7d81dc86406f2098187ecafde2562ce8756ebf2f
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-13 19:49:36 -0400

    dev

--- src/admin.js
+++ src/admin.js
@@ -8,14 +8,34 @@ export const runAdminSubmit = async (inputParams) => {
   const scrapeStats = await getScrapeStats(scrapeId);
   const returnObj = { ...scrapeStats, ...scrapeData };
   console.log("SCRAPE STATS");
-  console.log(scrapeStats);
+  console.log(returnObj);
   return returnObj;
 };
 
 //sends command to other app
+// export const sendAdminCommand = async (inputParams) => {
+//   try {
+//     const resAPI = await fetch(CONFIG.apiURL, {
+//       method: "POST",
+//       body: JSON.stringify(inputParams),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       //per claude fetch is weird with timeouts, prob better to use axios
+//       headersTimeout: 3 * 60 * 60 * 1000, // 2 hours (60 min × 60 sec × 1000 ms)
+//     });
+//     const data = await resAPI.json();
+//     console.log("DATA RETURN");
+//     console.log(data);
+//     return data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
 export const sendAdminCommand = async (inputParams) => {
   try {
-    const resAPI = await fetch(CONFIG.apiURL, {
+    const data = await fetch(CONFIG.apiURL, {
       method: "POST",
       body: JSON.stringify(inputParams),
       headers: {
@@ -24,9 +44,6 @@ export const sendAdminCommand = async (inputParams) => {
       //per claude fetch is weird with timeouts, prob better to use axios
       headersTimeout: 3 * 60 * 60 * 1000, // 2 hours (60 min × 60 sec × 1000 ms)
     });
-    const data = await resAPI.json();
-    console.log("DATA RETURN");
-    console.log(data);
     return data;
   } catch (e) {
     console.log(e);
