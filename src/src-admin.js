a6d0642e834c5844f032fe1866613b0a19117353
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-05-14 20:31:07 -0400

    trying axios instead of fetch to pass cors data

--- src/src-admin.js
+++ src/src-admin.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import CONFIG from "../config/config.js";
 import dbModel from "../models/db-model.js";
 
@@ -19,22 +21,34 @@ export const runAdminSubmit = async (inputParams) => {
   return scrapeObj;
 };
 
-// sends command to other app
+// FETCH VERSION
+// export const sendAdminCommand = async (inputParams) => {
+//   try {
+//     const resAPI = await fetch(CONFIG.apiURL, {
+//       method: "POST",
+//       body: JSON.stringify(inputParams),
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       //per claude fetch is weird with timeouts, prob better to use axios
+//       headersTimeout: 3 * 60 * 60 * 1000, // 2 hours (60 min × 60 sec × 1000 ms)
+//     });
+//     const data = await resAPI.json();
+//     console.log("DATA RETURN");
+//     console.log(data);
+//     return data;
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+//axios version, send command other app, NEED TO TEST
 export const sendAdminCommand = async (inputParams) => {
+  const { apiURL } = CONFIG;
   try {
-    const resAPI = await fetch(CONFIG.apiURL, {
-      method: "POST",
-      body: JSON.stringify(inputParams),
-      headers: {
-        "Content-Type": "application/json",
-      },
-      //per claude fetch is weird with timeouts, prob better to use axios
-      headersTimeout: 3 * 60 * 60 * 1000, // 2 hours (60 min × 60 sec × 1000 ms)
-    });
-    const data = await resAPI.json();
-    console.log("DATA RETURN");
-    console.log(data);
-    return data;
+    const res = await axios.post(apiURL, inputParams);
+
+    return res.data;
   } catch (e) {
     console.log(e);
   }
